1e2b2d5f9584211e2706f5980f1636c4d611e526
oaniba2734 <tomipraise11@gmail.com>
2026-02-08 17:27:39 -0500

    Create nested.py

---
+++ nested.py
@@ -0,0 +1,13 @@
+
+print("Weclome to the magic land")
+star_game = int (input ("\n Type 1 to play 0 to quit?:"))
+if star_game== 1:
+     print("You made it to the next level")
+     choice_1=input ("Type W to wait or S to swim accors the river?:") .upper()
+     if choice_1=="W":
+        print ("The boat is here")
+     else: 
+        print("Game is over you drown the river")
+else:
+    print("Thank you for your time")
+    
